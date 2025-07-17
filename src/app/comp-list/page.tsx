743c85b173c6bd77e2dc5414fff2125542be6f66
Dom <dom@together.agency>
2025-07-17 18:41:34 +0100

    Replace UserHeader with full Header component in comp list
    - Import and use the complete Header component from main page
    - Include logo on left and user dropdown on right
    - Match the header layout from the main page exactly
    - Fix header display to show proper navigation structure

--- src/app/comp-list/page.tsx
+++ src/app/comp-list/page.tsx
@@ -5,7 +5,7 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import ListSelector from '../../components/ListSelector'
 import ViewToggle, { ViewMode } from '../../components/ViewToggle'
-import UserHeader from '../../components/UserHeader'
+import Header from '../../components/Header'
 
 
 interface CompListItem {
@@ -564,7 +564,7 @@ export default function CompListPage() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-indigo-900">
-      <UserHeader />
+      <Header />
       
       {/* Progress Overlay */}
       {showProgressOverlay && (
