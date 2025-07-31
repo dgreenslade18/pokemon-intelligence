eb941748e5ef81c208b1076b3e507070c9aed9c6
Dom <dom@together.agency>
2025-07-31 21:07:22 +0100

    Add Google Tag Manager integration to Next.js layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Script from 'next/script'
 import './globals.css'
 import AuthSessionProvider from '../components/SessionProvider'
 import { ThemeProvider } from '../components/ThemeProvider'
@@ -31,7 +32,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${layGrotesk.variable} ${layGroteskHeading.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KTNX34HS');
+            `,
+          }}
+        />
+      </head>
       <body className="flex flex-col min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        
         <ThemeProvider>
           <AuthSessionProvider>
             <div className="flex-1">
